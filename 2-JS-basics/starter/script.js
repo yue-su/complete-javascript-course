4d24b8660cca1034895aeabe6fbc934fa98887b3
Yue Su <yuesu@Mac-Pro.local>
2020-06-06 13:27:30 -0300

    finishing challenge 1

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,21 @@
+let firstName = 'yue';
+console.log(firstName);
+
+
+
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+let bmiMark = massMark / heightMark^2;
+let bmiJohn = massJohn / heightJohn^2;
+
+console.log(bmiMark);
+console.log(bmiJohn);
+
+let result = bmiMark > bmiJohn;
+console.log(`Is mark's bmi larger than John's? ` + result);
+
+
